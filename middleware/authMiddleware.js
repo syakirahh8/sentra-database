2a5aff8e7d8b26e42f5e28e8b56546c0b32f2378
najlanafisah <najlanafisah11@gmail.com>
2026-04-09 16:12:12 +0700

    ganti supabase

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,33 +5,20 @@ const authenticateToken = async (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ 
-      status: 'error', 
-      message: 'Access denied. Tokennya mana? Login dulu yh!' 
-    });
+    return res.status(401).json({ status: 'error', message: 'Token tidak ditemukan!' });
   }
 
   try {
-    // verif token
     const { data: { user }, error } = await supabase.auth.getUser(token);
 
     if (error || !user) {
-      return res.status(403).json({ 
-        status: 'error', 
-        message: 'Invalid or expired session. Login lagi bes' 
-      });
+      return res.status(403).json({ status: 'error', message: 'Sesi tidak valid/expired!' });
     }
 
-    // simpan user ke moodcontroller
-    req.user = user;
+    req.user = user; // Lempar data user ke controller
     next();
-
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ 
-      status: 'error', 
-      message: 'Terjadi kesalahan pada server' 
-    });
+    res.status(500).json({ status: 'error', message: 'Server Error' });
   }
 };
 
