abd14b5812dc5a2df5aedde564d788dca9ce6d88
Azka Syakirah <azkasyakir800@gmail.comA>
2026-04-12 13:18:53 +0700

    ubah nama app.css jadi style.css

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "./app.csss";
+import "./style.css";
 
 // Assets
 import sentraIcon from "./assets/Sentra Icon.svg";
